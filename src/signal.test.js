12a08ac5a466509a2418b5ce5dd4e17af4eccee3
Ron Valstar <ron@ronvalstar.nl>
2023-05-19 08:15:51 +0200

    added test

--- src/signal.test.js
+++ src/signal.test.js
@@ -1,5 +1,40 @@
 import {createSignal} from './singal'
 
-test('adds 1 + 2 to equal 3', () => {
+test('expect `createSignal` to create object', () => {
   expect(createSignal(1, 2)).toBeDefined()
 })
+
+test('expect `createSignal` to create object', () => {
+
+  const earned = createSignal(0)
+  const spent = createSignal(0)
+  const totals = createSignal(0)
+
+  earned.add(n=>totals.dispatch(totals()+n))
+  spent.add(n=>totals.dispatch(totals()-n))
+  totals.add(console.log.bind(console,'totals'))
+
+  earned.dispatch(100) // :   0 + 100 = 100
+
+  expect(totals()).toBe(100)
+})
+
+
+
+//
+
+const earned = createSignal(0)
+const spent = createSignal(0)
+const totals = createSignal(0)
+
+earned.add(n=>totals.dispatch(totals()+n))
+spent.add(n=>totals.dispatch(totals()-n))
+totals.add(console.log.bind(console,'totals'))
+
+earned.dispatch(100) // :   0 + 100 = 100
+      .dispatch(50)  // : 100 +  50 = 150
+spent.dispatch(120)  // : 150 - 120 =  30            earned.dispatch(20)  // :  30 +  20 =  50
+console.log('totals()',totals()) // : 50
+
+
+
